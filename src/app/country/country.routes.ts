2298987c55ff8cd5f8efb529132a7e072e89441b
stanlyh <eduardostanly@gmail.com>
2025-05-01 22:44:09 -0500

    Inclusión de rutas desde otro archivo, inclusión del footer

---
+++ src/app/country/country.routes.ts
@@ -0,0 +1,22 @@
+import { Routes } from "@angular/router";
+import { CountryLayoutComponent } from "./layouts/CountryLayout/CountryLayout.component";
+import { ByCapitalPageComponent } from "./pages/by-capital-page/by-capital-page.component";
+
+export const countryRoutes: Routes = [
+  {
+    path: '',
+    component: CountryLayoutComponent,
+    children: [
+      {
+        path: 'by-capital',
+        component: ByCapitalPageComponent,
+      },
+      {
+        path: '**',
+        redirectTo: 'by-capital',
+      }
+    ]
+  },
+];
+
+export default countryRoutes;
---
+++ src/app/country/layouts/CountryLayout/CountryLayout.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-country-layout',
+  imports: [RouterOutlet],
+  templateUrl: './CountryLayout.component.html',
+})
+export class CountryLayoutComponent { }
