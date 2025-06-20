d1e460ebb494342bf1583a103ed7e9802636d39b
stanlyh <eduardostanly@gmail.com>
2025-06-19 23:25:09 -0500

    Busqueda por pais

--- src/app/country/pages/by-country-page/by-country-page.component.ts
+++ src/app/country/pages/by-country-page/by-country-page.component.ts
@@ -1,6 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, inject, resource, signal } from '@angular/core';
 import { SearchInputComponent } from "../../components/search-input/search-input.component";
 import { CountryListComponent } from "../../components/country-list/country-list.component";
+import { firstValueFrom } from 'rxjs';
+import { CountryService } from '../../services/country.service';
 
 @Component({
   selector: 'app-by-country-page',
@@ -9,5 +11,18 @@ import { CountryListComponent } from "../../components/country-list/country-list
 })
 export class ByCountryPageComponent {
 
+  countryService = inject(CountryService);
+  query = signal('');
+
+  countryResource = resource({
+    request: () => ({ query: this.query() }),
+    loader: async({ request }) => {
+      if ( !request.query ) return [];
+
+      return await firstValueFrom(
+        this.countryService.searchByCountry(request.query)
+      );
+    }
+  });
 
  }
