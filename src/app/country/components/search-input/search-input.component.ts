9ff5b0bbe3d3db76a92d8301c8e1f81f31ee7d92
stanlyh <eduardostanly@gmail.com>
2025-07-01 22:39:04 -0500

    Ejemplo busqueda debounce

--- src/app/country/components/search-input/search-input.component.ts
+++ src/app/country/components/search-input/search-input.component.ts
@@ -1,4 +1,4 @@
-import { Component, input, output } from '@angular/core';
+import { Component, effect, input, output, signal } from '@angular/core';
 
 @Component({
   selector: 'country-search-input',
@@ -9,5 +9,20 @@ export class SearchInputComponent {
 
   placeholder = input('Buscar');
   value = output<string>();
+  debounceTime = input(300);
+
+  inputValue = signal<string>('');
+
+  debounceEffect = effect((onCleanup) => {
+    const value = this.inputValue();
+
+    const timeout = setTimeout(()=> {
+      this.value.emit(value);
+    }, this.debounceTime());
+
+    onCleanup(()=> {
+      clearTimeout(timeout);
+    });
+  });
 
 }
--- src/app/country/services/country.service.ts
+++ src/app/country/services/country.service.ts
@@ -2,7 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
 
 import { RESTCountry } from '../interfaces/rest-countries.interfaces';
-import { map, Observable, catchError, throwError, delay } from 'rxjs';
+import { map, Observable, catchError, throwError, delay, of } from 'rxjs';
 import type { Country } from '../interfaces/country.interface';
 import { CountryMapper } from '../mappers/country.mappers';
 
@@ -17,6 +17,9 @@ export class CountryService {
 
   searchByCapital( query: string ): Observable<Country[]> {
     query = query.toLowerCase();
+
+    //console.log(`emitiendo valor ${query}`);
+    //return of([]);
 
     return this.http
       .get<RESTCountry[]>(`${API_URL}/capital/${query}`)
